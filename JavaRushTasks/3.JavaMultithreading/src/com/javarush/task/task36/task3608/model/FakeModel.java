a793d531cf30b974050ae0e0dada45acb834fef0
yury_kurensky <kurenskiy@gmail.com>
2021-02-12 10:39:21 +0300

    3608 3rd task

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+
+
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model{
+    private ModelData modelData = new ModelData();
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List users = new ArrayList<>();
+        users.add(new User("A", 1, 1));
+        users.add(new User("B", 2, 1));
+        users.add(new User("C", 3, 2));
+        modelData.setUsers(users);
+    }
+}
